a2b23040ce03ad5584a3f06bc32656b73339484d
Jason Davis <jasondavis303@gmail.com>
2020-07-22 09:43:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const github_token = core.getInput('github-token', {required: true});
-const version = core.getInput('version', {required: true});
-const octokit = github.getOctokit(github_token);
-const context = github.context;
 
-console.log(`Version: ${version}`);
-	
-function createTagIfNotExist()
+async function run()
 {
+	const github_token = core.getInput('github-token', {required: true});
+	const version = core.getInput('version', {required: true});
+	console.log(`Version: ${version}`);
+	
+	const octokit = github.getOctokit(github_token);
+	const context = github.context;
+	
 	var tag_exists = false;
 	octokit.paginate(octokit.repos.listTags, { ...context.repo },
 	(response, done) => 
 	{
-		console.log(`response.data: ${response.data}`);
 		if (response.data.find((tag) => tag.name == version))
 		{
+			console.log('Tag already exists');
 			tag_exists = true;
 			done();
 		}
 		return response.data;
 	});
 	
-	if(tag_exists)
-	{
-		console.log('Tag already exists');		
-	}
-	else
-	{
-		console.log('Creating tag');
-		octokit.git.createRef(
-		{ 
-			...context.repo, 
-			ref: `refs/tags/${version}`, 
-			sha: context.sha 
-		});
-	}
+	//if(!tag_exists)
+	//	octokit.git.createRef(
+	//	{ 
+	//		...context.repo, 
+	//		ref: `refs/tags/${version}`, 
+	//		sha: context.sha 
+	//	});
+		
+	
+	
 }
 
-createTagIfNotExist();
+run();
